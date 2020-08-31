551918e2c2711d9e926dec8ec4112c6deac9c821
EC2 Default User <ec2-user@ip-172-31-47-64.us-east-2.compute.internal>
2020-08-31 20:05:33 +0000

    changes for v2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World</h1>
+	<br>
+	<img src={"https://cdn.mos.cms.futurecdn.net/X5TyA8uvkGXoNyjFzxcowS-1200-80.jpg"}/>  
+	<h2>Created by Zain_Janjua</h2>
       </header>
     </div>
   );
